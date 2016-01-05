f83999ca2c874bb8c6976a87617598b1c6e7adec
enged9987 <enged9987@FHC-IC0026815.wrdsb.ca>
2016-01-05 10:55:28 -0500

    Fixing the Git

---
+++ src/PlatformswithPlayer/Main.java
@@ -0,0 +1,11 @@
+//David E 
+package PlatformswithPlayer;
+
+import PlatformScratch.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FraMain fraMain = new FraMain();
+    }
+}
---
+++ src/PlatformswithPlayer/Platforms.java
@@ -0,0 +1,33 @@
+package PlatformswithPlayer;
+
+import PlatformScratch.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+
+public class Platforms {
+
+    Dimension screenSize;
+    int nHeight, nWidth;
+    int nWidthR = 35, nLengthR = 300;
+    int nX, nY;
+
+    Platforms(int _nHeight, int _nWidth) {
+        nHeight = _nHeight;
+        nWidth = _nWidth;
+        nX = (int) (Math.random() * (nWidth - nLengthR));
+        nY = (nHeight / 5) * 4;
+    }
+
+    public void draw(Graphics2D g2D) {
+        g2D.fillRect(nX, nY, nLengthR, nWidthR);
+    }
+
+    public void update() {
+        nY++;
+        if (nY == nHeight) {
+            nX = (int) (Math.random() * (nWidth - nLengthR));
+            nY = 0;
+        }
+    }
+}
